d93a0a2c1261e809ff484377c5ff335dd2297d52
tiencoffee4 <tiencoffee4@gmail.com>
2023-04-10 20:14:57 +0700

    add snippets

--- build.js
+++ build.js
@@ -3,13 +3,25 @@ process.chdir(__dirname)
 const fs = require('fs-extra')
 const jsYaml = require('js-yaml')
 
-const dist = 'F:/apps/Microsoft VS Code/resources/app/extensions/livescript'
-const filename = 'livescript.tmLanguage'
+const vscodeExtsPath = 'F:/apps/Microsoft VS Code/resources/app/extensions'
 
-let json = jsYaml.load(fs.readFileSync(`./syntaxes/${filename}.yaml`, 'utf8'))
+if (fs.existsSync(vscodeExtsPath)) {
+   const vscodeExtsLiveScriptPath = vscodeExtsPath + '/livescript'
 
-fs.emptyDirSync(dist)
-fs.outputJsonSync(`${dist}/syntaxes/${filename}.json`, json)
+   fs.emptyDirSync(vscodeExtsLiveScriptPath)
 
-fs.copySync('./package.json', `${dist}/package.json`)
-fs.copySync('./language-configuration.json', `${dist}/language-configuration.json`)
+   let yamlText = fs.readFileSync('./syntaxes/livescript.tmLanguage.yaml', 'utf8')
+   let json = jsYaml.load(yamlText)
+   fs.outputJsonSync(`${vscodeExtsLiveScriptPath}/syntaxes/livescript.tmLanguage.json`, json)
+
+   json = fs.readJsonSync('./package.json')
+   fs.outputJsonSync(`${vscodeExtsLiveScriptPath}/package.json`, json)
+
+   json = fs.readJsonSync('./language-configuration.json')
+   fs.outputJsonSync(`${vscodeExtsLiveScriptPath}/language-configuration.json`, json)
+
+   json = fs.readJsonSync('./snippets/livescript.code-snippets')
+   fs.outputJsonSync(`${vscodeExtsLiveScriptPath}/snippets/livescript.code-snippets`, json)
+} else {
+   throw Error(`Không tìm thấy path: ${vscodeExtsPath}`)
+}
